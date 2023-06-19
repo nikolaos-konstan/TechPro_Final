3755fa8e957aa74d1f4c9cdc3ac0dec3cc1f6feb
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-06-19 12:14:41 +0300

    frontend restart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "./components/Navbar";
-import { Home } from "./routes/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
