1a4d8a0e0a96da85ed3e63330d44241f9c5340de
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-06-20 17:10:06 +0300

    generate random color

--- frontend/src/pages/Items.jsx
+++ frontend/src/pages/Items.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { ColorBox } from "../Items/ColorBox";
 import "./Items.css";
 
 export const Items = ({ items }) => {
+  const [item, setItem] = useState("");
+  const generateRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    setItem(color);
+    console.log(item);
+  };
+
   return (
     <div>
       <h1>Items</h1>
-      <button class="add-button">Add Colour</button>
+      <button onClick={generateRandomColor} className="add-button">
+        Add Colour
+      </button>
       <div className="box-container">
         {items.map((name) => (
           <ColorBox name={name.itemName} />
