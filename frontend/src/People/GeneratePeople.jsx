0337208bdcb13235230afd76162f1a4267d41511
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-06-20 14:49:24 +0300

    generate people update

--- frontend/src/People/GeneratePeople.jsx
+++ frontend/src/People/GeneratePeople.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const GeneratePeople = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("/generatedpeople.json")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(jsonData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  const generate = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/people", data);
+  };
+  //const generate = () => console.log(data);
   return (
     <div className="grid-item-3">
-      <button>Generate people</button>
+      <button onClick={generate}>Generate people</button>
     </div>
   );
 };
