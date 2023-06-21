a0aa5065b5772b37560a0ace6864f44aaa951af2
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-06-21 14:40:05 +0300

    Only God knows why this works, and I'm not God.

--- frontend/src/Orders/OrdersTable.jsx
+++ frontend/src/Orders/OrdersTable.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "../pages/Orders.css";
 import { OrderDetailsTable } from "./OrderDetailsTable";
 
 export const OrdersTable = ({ orders, orderDetails }) => {
+  const [filteredArray, setFilteredArray] = useState([]);
   //Function to reconstruct OrderDetails array
   const restructuredArray = orderDetails.map((obj) => {
     return {
@@ -9,14 +11,13 @@ export const OrdersTable = ({ orders, orderDetails }) => {
       orderId: obj.order.orderId,
       orderDetailsId: obj.orderDetailsId,
       itemId: obj.item.itemId,
+      itemName: obj.item.itemName,
       quantity: obj.quantity,
     };
   });
 
   const filterByOrderId = (arr, orderId) => {
-    console.log(arr);
-    const filteredArray = arr.filter((obj) => obj.orderId === orderId);
-    console.log(filteredArray);
+    setFilteredArray(arr.filter((obj) => obj.orderId === orderId));
   };
 
   return (
@@ -60,7 +61,33 @@ export const OrdersTable = ({ orders, orderDetails }) => {
         </table>
       </div>
       <div className="grid-item-orderDetails">
-        <OrderDetailsTable orderDetails={orderDetails} />
+        {5 > 2 ? <h1>Yes</h1> : <h1>NO</h1>}
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Order Number</th>
+                <th>Item</th>
+
+                <th>Quantity</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredArray.map((orderDetails) => (
+                <tr>
+                  <td>{orderDetails.orderDetailsId}</td>
+                  <td>{orderDetails.itemName}</td>
+                  <td>{orderDetails.quantity}</td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* <OrderDetailsTable orderDetails={orderDetails} /> */}
       </div>
     </div>
   );
