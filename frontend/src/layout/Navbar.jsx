c19b7c972fd50a0a7d249b4dd04498189d2af07e
Nikolaos Konstantinidis <nikolaos.konstan@gmail.com>
2023-06-23 13:55:51 +0300

    update

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "../App.css";
 
 export const Navbar = () => {
   return (
     <div>
-      <nav>
+      <nav className="navbar-header">
         <ul className="navbar">
-          <li className="section">
+          <li className="navbar-section">
             <Link to="/">Home</Link>
           </li>
-          <li className="section">
+          <li className="navbar-section">
             <Link to="/people">People</Link>
             <Link to="/allorders">All Orders</Link>
             <Link to="/items">All Items</Link>
           </li>
-          <li className="section">
+          <li className="navbar-section">
             <Link to="/about">About</Link>
           </li>
         </ul>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,66 @@
-import "./Home.css";
+import "../App.css";
 
 export const Home = ({ people, orders, items }) => {
   return (
-    <div className="container-cards">
-      <div className="card">
-        <h1 className="number">{people.length}</h1>
-        <p className="word">People Registered</p>
-      </div>
-      <div className="card">
-        <h1 className="number">{orders.length}</h1>
-        <p className="word">Orders Made</p>
-      </div>
-      <div className="card">
-        <h1 className="number">{items.length}</h1>
-        <p className="word">Items in Storage</p>
+    <div className="home-container">
+      <div className="home-container-cards">
+        <div className="home-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="card-icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+
+          <h1 className="home-card-number">{people.length}</h1>
+          <p className="home-card-word">People Registered</p>
+        </div>
+        <div className="home-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="card-icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            />
+          </svg>
+
+          <h1 className="home-card-number">{orders.length}</h1>
+          <p className="home-card-word">Orders Made</p>
+        </div>
+        <div className="home-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="card-icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+
+          <h1 className="home-card-number">{items.length}</h1>
+          <p className="home-card-word">Items in Storage</p>
+        </div>
       </div>
     </div>
   );
