e9e03551cf918d48ff48f9ca35f8d054149d5995
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-06-27 18:38:44 +0300

    about page done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,17 @@
-import "./About.css";
+import "../App.css";
 
 export const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-container">
+      <h1>About This Project</h1>
+      <p className="about-p">
+        This Web App was made by Nikolaos Konstantinidis, developed as part of
+        the TechPro Academy training program. It utilizes technologies such as
+        MySQL, Spring Boot with Maven for the backend, and React for the
+        frontend. It demonstrates the practical application of the knowledge
+        acquired during the training program. This academic project was
+        completed in June 2023.
+      </p>
     </div>
   );
 };
